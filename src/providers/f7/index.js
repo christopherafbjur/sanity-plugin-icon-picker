9b17e7fa59ed5bdd65714dbb2993167c6a07f71b
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-23 17:14:54 +0200

    Added react output format support for F7 icons

--- src/providers/f7/index.js
+++ src/providers/f7/index.js
@@ -1,19 +1,33 @@
 import React from "react";
+import decamelize from "decamelize";
 
 import styles from "framework7-icons";
-import iconList from "./icons.js";
+import * as F7 from "framework7-icons/react";
+
+function convertFormat(name, options) {
+  if (options.outputFormat === "react") return name;
+
+  const separator = "_";
+
+  //Separate letters followed by numbers (decamelize defaults to omitting separation of letter followed by number)
+  name = name.replace(/([a-z])([0-9])/i, `$1${separator}$2`);
+
+  return decamelize(name, { separator });
+}
 
 const iconStyle = { width: "20px", height: "20px", fontSize: "20px" };
 
 export default function (provider) {
-  return () =>
-    iconList.map((name) => {
+  return (options = {}) =>
+    Object.keys(F7).map((name) => {
+      const icon = convertFormat(name, {});
       return {
         provider,
-        name,
+        name: convertFormat(name, options),
         component: () => (
+          //Cannot simply generate the F7 SVG components here yet because of a bug: https://github.com/framework7io/framework7-icons/issues/34
           <i className={styles["f7-icons"]} style={iconStyle}>
-            {name}
+            {icon}
           </i>
         ),
       };
