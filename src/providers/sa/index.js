d3e95dd1116c6e9830e01de9df23fb62022ad1be
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-23 16:00:39 +0200

    Add output format support for Sanity Icons

--- src/providers/sa/index.js
+++ src/providers/sa/index.js
@@ -1,11 +1,17 @@
 import * as saIcons from "@sanity/icons";
 
+function convertFormat(icon, options) {
+  if (options.outputFormat === "react")
+    return icon[1].render.name.replace(/2$/, "");
+  return icon[0];
+}
+
 export default function (provider) {
-  return () =>
+  return (options = {}) =>
     Object.entries(saIcons.icons).map((icon) => {
       return {
         provider,
-        name: icon[0],
+        name: convertFormat(icon, options),
         component: () => icon[1].render({ width: "1.5em" }),
       };
     });
