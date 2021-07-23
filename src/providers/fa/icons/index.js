61f2660c4d7fbe19ce83e2973b256d096bc2fb3a
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-23 19:53:59 +0200

    Removed static icon names since we're now generating names automatically via react-icons

--- src/providers/fa/icons/index.js
+++
@@ -1,3 +0,0 @@
-import solid from "./solid";
-
-export default [...solid];
--- src/providers/mdi/icons/index.js
+++
@@ -1,9 +0,0 @@
-//From this cheat sheet https://pictogrammers.github.io/@mdi/font/5.4.55/
-import mdi from "./mdi";
-
-const missing = ["mdi-blank"];
-
-const filtered = [...mdi].filter(function (e) {
-  return this.indexOf(e) < 0;
-}, missing);
-export default filtered;
