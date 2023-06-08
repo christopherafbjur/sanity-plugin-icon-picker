52bd144c4b30c6426d6f4ec50fa21eb83a02fffa
Christopher Af Bjur <chrisdevcabc@gmail.com>
2023-06-08 14:16:06 +0200

    Init ResizeObserver and matchMedia mocks

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
+import { setupGlobalMatchMedia, setupGlobalResizeObserver } from './test/mocks';
+
+setupGlobalMatchMedia();
+setupGlobalResizeObserver();
