34e4f38a38b750ef0832892af980da50a9220f45
Christopher Af Bjur <chrisdevcabc@gmail.com>
2023-06-12 14:46:55 +0200

    Allow tabs children to be React element or React element array

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -2,7 +2,11 @@ import { Box, Container } from '@sanity/ui';
 import { TabsProvider } from './useTabs';
 import type { ReactElement } from 'react';
 
-export const Tabs = ({ children }: { children: ReactElement[] }) => {
+export const Tabs = ({
+  children,
+}: {
+  children: ReactElement | ReactElement[];
+}) => {
   return (
     <TabsProvider>
       <Container>
